bc867ec470d74c5319e6b0e0e5c2749a44b1adc7
alvittoag <alvittogenz09@gmail.com>
2023-05-27 16:49:05 +0700

    fix conflict

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import NavItemSlices from "./sidebar/NavItemSlices";
 import daftarKaSlices from "./daftar-ka/daftarKaSlices";
 import daftarHotelSlices from "./daftar-hotel/daftarHotelSlices";
-
+import daftarPenggunaSlices from "./daftar-pengguna/daftarPenggunaSlices";
 
 export const store = configureStore({
   reducer: {
     navItem: NavItemSlices,
     daftarKa: daftarKaSlices,
     daftarHotel: daftarHotelSlices,
-
+    daftarPengguna: daftarPenggunaSlices,
   },
 });
