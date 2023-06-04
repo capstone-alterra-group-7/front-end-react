01e599d9d9858b1803afe39365ba78b4c61f291f
fadlirizaldy <fadli.rizaldi12@gmail.com>
2023-06-04 14:45:55 +0700

    fix private route

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -5,13 +5,12 @@ import Swal from "sweetalert2";
 const PrivateRoute = () => {
   const token = sessionStorage.getItem("token");
 
-  // if (!token) {
-  //   Swal.fire("Gagal", "Token tidak valid", "error");
-  //   return <Navigate to="/" />;
-  // } else {
-  //   return <Outlet />;
-  // }
-  return <Outlet />;
+  if (!token) {
+    Swal.fire("Gagal", "Token tidak valid", "error");
+    return <Navigate to="/" />;
+  } else {
+    return <Outlet />;
+  }
 };
 
 export default PrivateRoute;
