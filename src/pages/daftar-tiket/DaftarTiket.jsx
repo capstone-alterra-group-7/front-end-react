05e1f51d59772958278e4b4c82fa8720c9666e74
alvittoag <alvittogenz09@gmail.com>
2023-06-10 13:04:05 +0700

    fix bar daftar ticket and fix time user dashboard

--- src/pages/daftar-tiket/DaftarTiket.jsx
+++ src/pages/daftar-tiket/DaftarTiket.jsx
@@ -1,25 +1,28 @@
-// ** Import Others
-import { useSelector } from "react-redux";
-
 // * Import Components
+import axios from "axios";
 import BarDaftarTiket from "../../components/daftar-tiket/BarDaftarTiket";
 import CardDaftarTiket from "../../components/daftar-tiket/CardDaftarTiket";
 
+// ** Import Other
+import { baseUrl } from "../../services/base";
+import useSWR from "swr";
+
+const fetcher = (url) => axios.get(url).then((res) => res.data);
 
 const DaftarTiket = () => {
-  const daftarTiket = useSelector((state) => state.daftarTiket)
+  const { data: daftarTicket, isLoading } = useSWR(
+    baseUrl(`/admin/order/ticket`),
+    fetcher
+  );
+
   return (
     <div>
-      <BarDaftarTiket/>
-      <div className="">
-
-        {/* {daftarTiket.map((tiket, index) => (
-          <CardDaftarTiket data={tiket} index={index}/>
-        ))} */}
-        <CardDaftarTiket/>
-      </div>
+      <BarDaftarTiket />
+      {daftarTicket?.data?.map((ticket) => (
+        <CardDaftarTiket />
+      ))}
     </div>
-  )
+  );
 };
 
 export default DaftarTiket;
