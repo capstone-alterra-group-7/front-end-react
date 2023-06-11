b100f488929126c27e4f3f826e4228cc49907c4e
alvittoag <alvittogenz09@gmail.com>
2023-06-11 16:04:33 +0700

    tiket connect api and pesanan ka connect api

--- src/components/dashboard/CardPesananBaru.jsx
+++ src/components/dashboard/CardPesananBaru.jsx
@@ -1,71 +1,113 @@
-import React from "react";
-import assets from "../../assets/assets";
-
-
+import moment from "moment";
 
 const CardPesananBaru = ({ dataPesananBaru }) => {
-
-    return (
-        <div className="px-[32px] pb-[24px] col-span-4">
-            <div className="pb-[24px] rounded-[16px] bg-[#FFFFFF] px-[32px] flex border-2 border-[#E1E4EA] ">
-                <div className="flex justify-between">
-                    <div className=" text-left ">
-                        <div className="">
-                            <h1 className="font-[600] text-[20px] mt-[24px] mb-[16px]">Pesanan Baru</h1>
-                        </div>
-
-                        <div className="rounded-xl border border-[#D2D7E0]">
-                            <table className="w-full text-left table-fixed">
-                                <thead>
-                                    <tr>
-                                        <th scope="col" className="px-6 py-[20px] font-bold w-14">
-                                            No
-                                        </th>
-                                        <th scope="col" className="px-6 py-[20px] font-bold w-52">
-                                            Pesanan
-                                        </th>
-                                        <th scope="col" className="px-6 py-[20px] font-bold text-center">
-                                            Tipe
-                                        </th>
-                                        <th scope="col" className="px-2 py-[20px] font-bold text-center">
-                                            Nomor Pesanan
-                                        </th>
-                                        <th scope="col" className="px-3 py-[20px] font-bold text-center">
-                                            Harga
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {dataPesananBaru?.data?.new_order ? (
-                                        [...Array(10)].map((data, idx) => {
-                                            return (
-                                                <tr className={`border-t border-[#D2D7E0] ${idx % 2 == 0 ? "bg-[#F9FAFB]" : "bg-white"}`}>
-                                                    <td className="px-7 py-[8px] rounded-bl-xl">{idx + 1}.</td>
-                                                    <td className="px-6 py-[8px] flex flex-col">
-                                                        <h1 className="font-semibold mb-1">Putri Deli</h1>
-                                                        <h1 className="text-sm">16 Apr 2023 07:21 WIB</h1>
-                                                    </td>
-                                                    <td className=" py-[8px]  font-medium">
-                                                        <div className={`py-[6px] text-center font-semibold px-6 rounded-[24px] ${idx % 2 == 0 ? "bg-[#FFF1E5] text-[#FF7300]" : "bg-[#E5F2FF] text-[#0080FF]"}`}>{idx % 2 == 0 ? "Hotel" : "Kereta"}</div>
-                                                    </td>
-                                                    <td className="px-2 py-[8px] text-center font-[500]">123456789</td>
-                                                    <td className="px-1 py-[8px] rounded-br-xl text-center font-[500] 2xl:text-base text-sm">Rp 12.000.000</td>
-                                                </tr>
-                                            );
-                                        }
-                                        )
-                                    ) : ""}
-
-                                </tbody>
-                            </table>
-                        </div>
-                        {dataPesananBaru?.data?.new_order ? "" : (
-                            <h1 className="text-bold text-gray-600 text-[26px] text-center mt-6">No Data</h1>
-                        )}
-                    </div>
-                </div>
+  console.log(dataPesananBaru);
+  return (
+    <div className="px-[32px] pb-[24px] col-span-4">
+      <div className="pb-[24px] rounded-[16px] bg-[#FFFFFF] px-[32px] flex border-2 border-[#E1E4EA] ">
+        <div className="flex justify-between">
+          <div className=" text-left ">
+            <div className="">
+              <h1 className="font-[600] text-[20px] mt-[24px] mb-[16px]">
+                Pesanan Baru
+              </h1>
             </div>
+
+            <div className="rounded-xl border border-[#D2D7E0]">
+              <table className="w-full text-left table-fixed">
+                <thead>
+                  <tr>
+                    <th scope="col" className="px-6 py-[20px] font-bold w-14">
+                      No
+                    </th>
+                    <th scope="col" className="px-6 py-[20px] font-bold w-52">
+                      Pesanan
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-[20px] font-bold text-center"
+                    >
+                      Tipe
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-2 py-[20px] font-bold text-center"
+                    >
+                      Nomor Pesanan
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-3 py-[20px] font-bold text-center"
+                    >
+                      Harga
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {dataPesananBaru?.data?.new_order?.map((data, idx) => (
+                    <tr
+                      key={idx}
+                      className={`border-t border-[#D2D7E0] ${
+                        idx % 2 == 0 ? "bg-[#F9FAFB]" : "bg-white"
+                      }`}
+                    >
+                      <td className="px-7 py-[8px] rounded-bl-xl">
+                        {idx + 1}.
+                      </td>
+                      <td className="px-6 py-[8px] flex flex-col">
+                        <h1 className="font-semibold mb-1">
+                          {data.order_name}
+                        </h1>
+                        <h1 className="text-sm">
+                          {moment(data.created_at).format("D MMM YYYY")}{" "}
+                          <span className="ml-2">
+                            {new Date(data.created_at).toLocaleTimeString(
+                              "id",
+                              {
+                                hour: "2-digit",
+                                minute: "2-digit",
+                              }
+                            )}{" "}
+                          </span>
+                          WIB
+                        </h1>
+                      </td>
+                      <td className=" py-[8px]  font-medium">
+                        <div
+                          className={`py-[6px] text-center font-semibold px-6 rounded-[24px] ${
+                            idx % 2 == 0
+                              ? "bg-[#FFF1E5] text-[#FF7300]"
+                              : "bg-[#E5F2FF] text-[#0080FF]"
+                          }`}
+                        >
+                          {data.type}
+                        </div>
+                      </td>
+                      <td className="px-2 py-[8px] text-center font-[500]">
+                        {data.booking_number.substring(16, 24)}
+                      </td>
+                      <td className="px-1 py-[8px] rounded-br-xl text-center font-[500] 2xl:text-base text-sm">
+                        {new Intl.NumberFormat("id-ID", {
+                          style: "currency",
+                          currency: "IDR",
+                        }).format(data.price)}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            {dataPesananBaru?.data?.new_order ? (
+              ""
+            ) : (
+              <h1 className="text-bold text-gray-600 text-[26px] text-center mt-6">
+                No Data
+              </h1>
+            )}
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 export default CardPesananBaru;
--- src/components/pesanan-ka/detail-pesananKa/PriceDetail.jsx
+++ src/components/pesanan-ka/detail-pesananKa/PriceDetail.jsx
@@ -1,35 +1,41 @@
-import assets from "../../../assets/assets"
-
 export default function PriceDetail({ data }) {
   return (
-      <div className="bg-white py-[16px] px-[32px] mt-2">
-                
-                <p className="text-[20px] font-[600] mb-3">Rincian Harga</p>
-                <div className="border border-[#D2D7E0] rounded-2xl p-[16px]">
-
-                    <div className="flex justify-between mb-2">
-                        <p>{data.train_name}</p>
-                        <p className='font-[600]'>Rp. {data.ticket_price}</p>
-                    </div>
-                    <hr />
-
-                    <div className="flex justify-between my-2">
-                        <p>Biaya Layanan</p>
-                        <p className='font-[600]'>Rp. {data.service_fee}</p>
-                    </div>
-                    <hr />
-
-                    <div className="flex justify-between my-2">
-                        <p>Asuransi Kereta Api</p>
-                        <p className='font-[600]'>Rp. {data.insurance}</p>
-                    </div>
-                    <hr />
-                    
-                    <div className="flex justify-between my-2">
-                        <p className='text-[20px] font-[600] text-[#4B4C4E]'>Total Akhir</p>
-                        <p className='text-[#0080FF] text-[20px] font-[600]'>Rp. {data.price}</p>
-                    </div>
-                </div>
+    <div className="bg-white py-[16px] px-[32px] mt-2">
+      <p className="text-[20px] font-[600] mb-3">Rincian Harga</p>
+      <div className="border border-[#D2D7E0] rounded-2xl p-[16px]">
+        <div className="flex justify-between mb-2">
+          <p>{data.train.name}</p>
+          <p className="font-[600]">
+            {new Intl.NumberFormat("id-ID", {
+              style: "currency",
+              currency: "IDR",
+            }).format(data.train.train_price)}
+          </p>
         </div>
-  )
+        <hr />
+
+        <div className="flex justify-between my-2">
+          <p>Biaya Layanan</p>
+          <p className="font-[600]">Rp. 0</p>
+        </div>
+        <hr />
+
+        <div className="flex justify-between my-2">
+          <p>Asuransi Kereta Api</p>
+          <p className="font-[600]">Rp. 0</p>
+        </div>
+        <hr />
+
+        <div className="flex justify-between my-2">
+          <p className="text-[20px] font-[600] text-[#4B4C4E]">Total Akhir</p>
+          <p className="text-[#0080FF] text-[20px] font-[600]">
+            {new Intl.NumberFormat("id-ID", {
+              style: "currency",
+              currency: "IDR",
+            }).format(data.train.train_price)}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
