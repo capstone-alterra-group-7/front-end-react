3941eb9d2450547761ee7732e98323ab92306682
alvittoag <alvittogenz09@gmail.com>
2023-06-13 15:51:01 +0700

    fix error deploy

--- src/components/daftar-hotel/detail-hotel/InformasiHotel.jsx
+++ src/components/daftar-hotel/detail-hotel/InformasiHotel.jsx
@@ -9,7 +9,12 @@ import MapComponent from "./MapComponent";
 import ImageHotelSection from "./ImageHotelSection";
 
 const InformasiHotel = ({ data }) => {
-  const [isHidden, setIsHidden] = useState({ fasilitas: true, desc: false, informasi: false, kebijakan: false });
+  const [isHidden, setIsHidden] = useState({
+    fasilitas: true,
+    desc: false,
+    informasi: false,
+    kebijakan: false,
+  });
 
   const rupiah = (number) => {
     if (!number) return "Rp 0";
@@ -27,7 +32,9 @@ const InformasiHotel = ({ data }) => {
 
         <div className="w-full">
           <div className="flex justify-between mb-2">
-            <h1 className="text-[#262627] font-bold text-[2rem]">{data?.name}</h1>
+            <h1 className="text-[#262627] font-bold text-[2rem]">
+              {data?.name}
+            </h1>
             <div className="text-end flex justify-end">
               {[...Array(5)].map((none, i) => {
                 if (i <= Math.floor(parseInt(data?.class)) - 1) {
@@ -38,11 +45,15 @@ const InformasiHotel = ({ data }) => {
             </div>
           </div>
 
-          <h1 className="text-[#0080FF] font-semibold text-xl mb-2">{data?.id}</h1>
+          <h1 className="text-[#0080FF] font-semibold text-xl mb-2">
+            {data?.id}
+          </h1>
 
           <div className="flex items-center mb-3">
             <img src={assets.iconGpsMarker} alt="" className="mr-2" />
-            <h1 className="text-xl font-medium text-[#262627]">{data?.address}</h1>
+            <h1 className="text-xl font-medium text-[#262627]">
+              {data?.address}
+            </h1>
           </div>
 
           <div className="flex items-center mb-28">
@@ -55,25 +66,37 @@ const InformasiHotel = ({ data }) => {
           <h1 className="text-xl font-medium mb-2">Harga mulai dari</h1>
 
           <h1 className="text-[2.5rem] font-bold font-sans text-[#0080FF]">
-            {rupiah(data?.price)} <span className="text-xl text-[#96989C]">/ kamar / malam</span>
+            {rupiah(data?.price)}{" "}
+            <span className="text-xl text-[#96989C]">/ kamar / malam</span>
           </h1>
         </div>
       </div>
 
-      <div className={`mt-2 bg-white ${isHidden.fasilitas ? "h-[19rem]" : ""} p-8`}>
+      <div
+        className={`mt-2 bg-white ${isHidden.fasilitas ? "h-[19rem]" : ""} p-8`}
+      >
         <h1 className="text-xl font-bold mb-6">Fasilitas Umum</h1>
 
         <div className="pt-5 grid grid-cols-8 gap-y-3 ps-10">
           {data?.hotel_facilities.length < 1
             ? "No Facilities"
-            : data?.hotel_facilities.slice(0, isHidden.fasilitas ? 8 : data?.hotel_facilities.length).map((facility, i) => {
-                return (
-                  <div className="w-24 flex flex-col items-center" key={i}>
-                    <img src={assets.iconWifi} alt="iconwifi" className="w-10" />
-                    <h1 className="text-center">{facility?.name}</h1>
-                  </div>
-                );
-              })}
+            : data?.hotel_facilities
+                .slice(
+                  0,
+                  isHidden.fasilitas ? 8 : data?.hotel_facilities.length
+                )
+                .map((facility, i) => {
+                  return (
+                    <div className="w-24 flex flex-col items-center" key={i}>
+                      <img
+                        src={assets.iconWifi}
+                        alt="iconwifi"
+                        className="w-10"
+                      />
+                      <h1 className="text-center">{facility?.name}</h1>
+                    </div>
+                  );
+                })}
           {/* {[...Array(isHidden.fasilitas ? 8 : 18)].map((none, i) => {
             return (
               <div className="w-24 flex flex-col justify-center items-center" key={i}>
@@ -92,8 +115,14 @@ const InformasiHotel = ({ data }) => {
             })
           }
         >
-          <h1 className="text-[#0080FF] font-medium mr-2">{isHidden.fasilitas ? "Lihat Semua Fasilitas Hotel" : "Hide"}</h1>
-          <img src={assets.iconDownArrow} alt="arrow" className={`${isHidden.fasilitas ? "" : "rotate-180 duration-200"}`} />
+          <h1 className="text-[#0080FF] font-medium mr-2">
+            {isHidden.fasilitas ? "Lihat Semua Fasilitas Hotel" : "Hide"}
+          </h1>
+          <img
+            src={assets.iconDownArrow}
+            alt="arrow"
+            className={`${isHidden.fasilitas ? "" : "rotate-180 duration-200"}`}
+          />
         </div>
       </div>
 
@@ -108,9 +137,17 @@ const InformasiHotel = ({ data }) => {
             }
           >
             <h1 className="ms-4 font-semibold">Deskripsi Hotel</h1>
-            <img src={assets.iconUrutkanDaftarKa} alt="" className={`h-5 w-4 duration-300 ${isHidden.desc ? "rotate-180" : ""}`} />
+            <img
+              src={assets.iconUrutkanDaftarKa}
+              alt=""
+              className={`h-5 w-4 duration-300 ${
+                isHidden.desc ? "rotate-180" : ""
+              }`}
+            />
           </div>
-          {isHidden.desc ? null : <div className="p-4 duration-500">{data?.description}</div>}
+          {isHidden.desc ? null : (
+            <div className="p-4 duration-500">{data?.description}</div>
+          )}
         </div>
 
         <div className="border-2 border-[#E1E4EA] rounded-lg pt-2 mb-8">
@@ -123,7 +160,13 @@ const InformasiHotel = ({ data }) => {
             }
           >
             <h1 className="ms-4 font-semibold">Informasi Hotel</h1>
-            <img src={assets.iconUrutkanDaftarKa} alt="" className={`h-5 w-4 duration-300 ${isHidden.informasi ? "rotate-180" : ""}`} />
+            <img
+              src={assets.iconUrutkanDaftarKa}
+              alt=""
+              className={`h-5 w-4 duration-300 ${
+                isHidden.informasi ? "rotate-180" : ""
+              }`}
+            />
           </div>
           {isHidden.informasi ? null : <SectionInformation data={data} />}
         </div>
@@ -138,15 +181,23 @@ const InformasiHotel = ({ data }) => {
             }
           >
             <h1 className="ms-4 font-semibold">Kebijakan Hotel</h1>
-            <img src={assets.iconUrutkanDaftarKa} alt="" className={`h-5 w-4 duration-300 ${isHidden.kebijakan ? "rotate-180" : ""}`} />
+            <img
+              src={assets.iconUrutkanDaftarKa}
+              alt=""
+              className={`h-5 w-4 duration-300 ${
+                isHidden.kebijakan ? "rotate-180" : ""
+              }`}
+            />
           </div>
-          {isHidden.kebijakan ? null : <SectionKebijakan data={data?.hotel_policy} />}
+          {isHidden.kebijakan ? null : (
+            <SectionKebijakan data={data?.hotel_policy} />
+          )}
         </div>
 
-        <div className="mt-16">
+        {/* <div className="mt-16">
           <h1 className="font-bold text-xl mb-6">Lokasi Hotel</h1>
           <MapComponent isMarkerShown />
-        </div>
+        </div> */}
       </div>
     </div>
   );
