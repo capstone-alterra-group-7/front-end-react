852db979770432962cb76b5f97f728fc6570ef13
alvittoag <alvittogenz09@gmail.com>
2023-06-17 23:50:23 +0700

    add pagination pesanan hotel

--- src/components/pesanan-hotel/CardContainerPesananHotel.jsx
+++ src/components/pesanan-hotel/CardContainerPesananHotel.jsx
@@ -1,19 +1,7 @@
 // ** Import Components
 import CardPesananHotel from "./CardPesananHotel";
 
-// ** Import Other
-import { baseUrl } from "../../services/base";
-import useSWR from "swr";
-import axios from "axios";
-
-const fetcher = (url) => axios.get(url).then((res) => res.data);
-
-const CardContainerPesananHotel = () => {
-  const { data: daftarHotel, isLoading } = useSWR(
-    baseUrl(`/admin/order/hotel`),
-    fetcher
-  );
-
+const CardContainerPesananHotel = ({ daftarHotel }) => {
   return (
     <div className="py-8 px-7">
       {daftarHotel?.data?.map((data, idx) => {
--- src/pages/pesanan-hotel/PesananHotel.jsx
+++ src/pages/pesanan-hotel/PesananHotel.jsx
@@ -3,20 +3,43 @@ import React, { useState } from "react";
 
 // Import Components
 import HeaderPesananHotel from "../../components/pesanan-hotel/Header";
-
+import BackDetailHotel from "../../components/pesanan-hotel/detail-pesananHotel/BackDetailHotel";
 import CardContainerPesananHotel from "../../components/pesanan-hotel/CardContainerPesananHotel";
 
 // ** Import Assets
+import { baseUrl } from "../../services/base";
+import axios from "axios";
+import useSWR from "swr";
+import Pagination from "../daftar-KA/Pagination";
+
+const fetcher = (url) => axios.get(url).then((res) => res.data);
 
 const PesananHotel = () => {
   const [modal, setModal] = useState(false);
+  const [changePage, setChangePage] = useState(1);
+
+  const { data: daftarHotel, isLoading } = useSWR(
+    baseUrl(`/admin/order/hotel?page=${changePage}`),
+    fetcher
+  );
+
+  const infoPaginate = daftarHotel?.meta;
+
+  console.log(infoPaginate);
 
   return (
     <div className="relative">
       <div>
         <HeaderPesananHotel setModal={setModal} />
       </div>
-      <CardContainerPesananHotel />
+      <CardContainerPesananHotel daftarHotel={daftarHotel} />
+
+      <Pagination
+        changePage={changePage}
+        setChangePage={setChangePage}
+        isLoading={isLoading}
+        infoPaginate={infoPaginate}
+      />
     </div>
   );
 };
