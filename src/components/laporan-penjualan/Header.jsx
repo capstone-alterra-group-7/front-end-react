d68859a01d07d2c77fb0b89954755da51dac59a2
alvittoag <alvittogenz09@gmail.com>
2023-06-24 15:59:35 +0700

    delete package

--- src/components/laporan-penjualan/Header.jsx
+++ src/components/laporan-penjualan/Header.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import assets from "../../assets/assets";
-import { CSVLink } from "react-csv";
 
 const Header = ({ month, setMonth }) => {
   // Example export csv
-  const headers = [
-    { label: "First Name", key: "firstname" },
-    { label: "Last Name", key: "lastname" },
-    { label: "Email", key: "email" },
-  ];
+  // const headers = [
+  //   { label: "First Name", key: "firstname" },
+  //   { label: "Last Name", key: "lastname" },
+  //   { label: "Email", key: "email" },
+  // ];
 
-  const data = [
-    { firstname: "Ahmed", lastname: "Tomi", email: "ah@smthing.co.com" },
-    { firstname: "Raed", lastname: "Labes", email: "rl@smthing.co.com" },
-    { firstname: "Yezzi", lastname: "Min l3b", email: "ymin@cocococo.com" },
-  ];
+  // const data = [
+  //   { firstname: "Ahmed", lastname: "Tomi", email: "ah@smthing.co.com" },
+  //   { firstname: "Raed", lastname: "Labes", email: "rl@smthing.co.com" },
+  //   { firstname: "Yezzi", lastname: "Min l3b", email: "ymin@cocococo.com" },
+  // ];
 
   return (
     <>
@@ -23,7 +22,11 @@ const Header = ({ month, setMonth }) => {
       </div>
       <div className="px-8 flex justify-end gap-6">
         <div className="relative">
-          <img src={assets.iconCalendar} alt="" className="absolute top-[10px] left-3" />
+          <img
+            src={assets.iconCalendar}
+            alt=""
+            className="absolute top-[10px] left-3"
+          />
           <select
             className="w-36 h-11 ps-11 py-[10px] flex justify-center font-medium rounded-lg border border-[#D2D7E0] focus:border-[#0080FF] focus:ring-[#0080FF]"
             onChange={(e) => setMonth(e.target.value)}
@@ -35,10 +38,10 @@ const Header = ({ month, setMonth }) => {
             <option value="Des 2023">Des 2023</option>
           </select>
         </div>
-        <CSVLink data={data} headers={headers} className="bg-[#0080FF] rounded-lg h-11 px-4 font-medium py-[10px] text-white flex items-center gap-2">
+        {/* <CSVLink data={data} headers={headers} className="bg-[#0080FF] rounded-lg h-11 px-4 font-medium py-[10px] text-white flex items-center gap-2">
           <img src={assets.iconFileExport} alt="" className="w-4 h-4" />
           <h1>Export CSV</h1>
-        </CSVLink>
+        </CSVLink> */}
       </div>
     </>
   );
